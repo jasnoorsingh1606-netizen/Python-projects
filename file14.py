5fff9f7a06a1ad7c7c0bd8a58b38b627c5acdfb1
jasnoorsingh1606-netizen <jasnoorsingh1606@gmail.com>
2025-10-19 10:47:28 +0530

    Alphabet printing project

---
+++ file14.py
@@ -0,0 +1,200 @@
+# for i in range(1,5):
+#     for j in range (1,10):
+#         if ((i == 1 and j==5 ) or ((i == 2 and (j == 4 or j==6))) or ((i == 3 and (j == 3 or j==5 or j==7 ))) or ((i == 4 and (j == 2 or j==8)))) : 
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()
+
+# for i in range(1,8):
+#     for j in range (1,6):
+#         if ((i == 1 and (j==2 or j ==3 or j==4 )) or((i == 2 or i==3 or i==5 or i==6) and (j==2 or j ==5)) or (i == 4 and (j==2 or j==4 )) or (i == 7 and (j==2 or j ==3 or j==4 ))) : 
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()
+
+# for i in range(1,8):
+#     for j in range (1,6):
+#         if (((i == 1 or i == 7) and (j == 2 or j == 4)) or ((i == 2 or i == 6) and j == 5) or ((i == 3 or i == 5) and j==1)):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()    
+
+# for i in range(1,8):
+#     for j in range (1,6):
+#         if (((i == 1 or i == 7) and (j == 2 or j == 3 or j==4 )) or ((i == 2 or i == 3 or i ==6) and j == 2 ) or ((i == 3 or i == 5) and (j == 2 or j == 5 ))):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()    
+
+# for i in range(1,7):
+#     for j in range (1,6):
+#         if (((i == 1 or i == 3  ) and (j == 2 or j ==3 or j == 4)) or ((i == 2 or i == 3 or i ==4 or i == 5) and j == 2)):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()    
+
+# for i in range(1,8):
+#     for j in range (1,6):
+#         if ((i == 1 and (j == 2 or j == 3)) or ((i == 2 or i == 6) and (j == 1 or j == 4)) or ((i == 3 or i == 4) and j == 1) or (i == 5 and (j == 1 or j == 3 or j == 4 or j == 5)) or (i == 7 and (j == 1 or j == 2 or j == 3 or j == 4))):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()    
+
+# for i in range(1,8):
+#     for j in range (1,5):
+#         if ((i == 1 or i == 2 or i == 3 or i == 5 or i == 6 or i==7) and (j == 1 or j == 4)) or ((i == 4 and (j == 1 or j ==2 or j==3 or j == 4))) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,6):
+#     for j in range (1,4):
+#         if ((i == 1 or i == 5) and (j == 1 or j == 2 or j ==3 )) or ((i == 2 or i == 3 or i == 4) and j == 2) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,7):
+#     for j in range (1,6):
+#         if ((i == 1  and (j == 1 or j == 2 or j == 3 or j == 4 or  j == 5)) or ((i == 2 or i == 3 or i == 4)  and j == 3) or ((i == 5 or i == 6)  and (j == 1 or j == 3 )))  :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,8):
+#     for j in range  (1,6):
+#         if ((i == 1 or i == 7) and (j == 1 or  j == 5)) or ((i == 2 or i == 6 )  and (j == 1 or j == 4)) or ((i == 3 or i == 5)  and (j == 1 or j == 3 )) or (i == 4  and (j == 1 or j == 2 )) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,7):
+#     for j in range  (1,5):
+#         if ((i == 1 or i == 2 or i == 3 or i == 4 or i == 5) and j == 1) or ( i == 6 and (j == 1 or j == 2 or j == 3 or j == 4)):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,7):
+#     for j in range  (1,6):
+#         if ((i == 1 or i == 4 or i == 5 or i == 6) and (j == 1 or j == 5)) or ( i == 2 and (j == 1 or j == 2 or j == 4 or j == 5)) or (i == 3 and (j == 1 or j == 3 or j == 5 )) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,6):
+#     for j in range (1,6):
+#         if (((i == 1 or i == 5) and (j == 1 or  j == 5 )) or(i == 2  and (j == 1  or j == 2 or j == 5 )) or (i == 3 and (j == 1  or j == 3 or j == 5 )) or (i == 4 and (j == 1  or j == 4 or j == 5 ))) : 
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()
+
+# for i in range(1,7):
+#     for j in range  (1,6):
+#         if ((i == 1 or i == 6) and (j == 2 or j == 3 or j == 4)) or (i == 2 or i == 3 or i == 4 or i == 5 ) and (j == 1  or j == 5):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,9):
+#     for j in range  (1,5):
+#         if ((i == 1 or i == 5) and (j == 1 or j == 2 or j == 3)) or (i == 2 or i == 3) and (j == 1  or j == 4) or ((i == 4  or i == 6 or i == 7 or i==8) and j == 1) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,7):
+#     for j in range  (1,6):
+#         if (i == 1  and (j == 2 or j == 3)) or (i == 2 or i == 3 or i == 5) and (j == 1  or j == 4) or (i == 4   and (j == 1 or j == 3 or j == 4 )) or (i == 6 and (j == 2 or j == 3 or j == 5)):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()  
+
+# for i in range(1,8):
+#     for j in range  (1,6):
+#         if ((i == 1 or i == 4)  and ( j == 1 or j == 2 or j == 3)) or (i == 2 or i == 3 or i == 6) and (j == 1  or j == 4) or (i == 5 and (j == 1 or j == 3)) or (i == 7 and (j == 1 or  j == 5)):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,10):
+#     for j in range  (1,5):
+#         if ((i == 1 or i == 5 or i == 9)  and ( j == 2 or j == 3)) or ((i == 2 or i == 3 or i == 4) and j == 1) or ((i == 6 or i == 7 or i == 8) and j == 4) :
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,8):
+#     for j in range  (1,6):
+#         if (i == 1   and ( j==1 or j == 2 or j == 3 or j ==4 or j == 5 )) or ((i == 2 or i == 3 or i == 4 or i == 5 or i == 6 or i == 7 ) and j == 3):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+for i in range(1,7):
+    for j in range  (1,6):
+        if ((i == 1 or i == 2 or i == 3 or i == 4 or i == 5)    and ( j==1 or j == 5  )) or ( i == 6  and (j == 2 or j == 3 or j == 4 )):
+            print(" *" , end = "")
+        else:
+           print("  " , end = "")
+    print() 
+    
+# for i in range(1,5):
+#     for j in range  (1,8):
+#         if (i == 1  and ( j==1 or j == 7 )) or ( i == 2  and (j == 2 or j == 6 )) or (i == 3  and (j == 3 or j == 5 )) or ( i == 4  and j == 4 ):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,5):
+#     for j in range  (1,12):
+#         if (i == 1  and ( j==1 or j == 11 )) or ( i == 2  and (j == 2 or j == 6  or j == 10 )) or (i == 3  and (j == 3 or j == 5 or j == 7 or j == 9 )) or ( i == 4  and (j == 4 or j == 8 )):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()     
+
+# for i in range(1,6):
+#     for j in range  (1,6):
+#         if ((i == 1  or i == 5) and (j == 1 or j == 5)) or ((i == 2 or i == 4) and (j == 2 or j == 4)) or (i == 3 and j == 3):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,9):
+#     for j in range  (1,8):
+#         if (i == 1  and ( j==1 or j == 7 )) or ( i == 2  and (j == 2 or j == 6 )) or (i == 3  and (j == 3 or j == 5 )) or ((i == 4 or i == 5 or i == 6 or i == 7 or i == 8)  and j == 4):
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print() 
+
+# for i in range(1,6):
+#     for j in range  (1,6):
+#         if ((i == 1 or i == 5) and (j == 1 or j == 2 or j == 3 or j == 4 or j == 5)) or ( i == 2  and j == 4) or (i == 3  and j == 3) or (i == 4 and j == 2): 
+#             print(" *" , end = "")
+#         else:
+#            print("  " , end = "")
+#     print()
+
